16c76b9c6dee24aba517dba9287d46ed9c2eaa73
Adrien Baudet <baudetad@gmail.com>
2019-05-12 10:26:11 +0200

    finished base blog

--- src/article.rb
+++ src/article.rb
@@ -2,20 +2,36 @@ require 'json'
 require 'pandoc-ruby'
 
 class Article
+  attr_reader :path, :title
   def initialize(path, template_path)
     # definit le chemin de l'article
     @path = path
 
     # charge le template
-    @template = File.open(template_path, "r"){|file| file.read}
+    template = File.open(template_path, "r"){|file| file.read}
 
     # charge le fichier config
     config = File.open("#{@path}/config.json", "r"){|file| file.read}
     @config = JSON.parse(config)
     @config.default = ""
 
+    # definit le titre
+    @title = @config["title"]
+
     # charge l'article
     article = File.open("#{@path}/#{@config["article"]}", "r"){|file| file.read}
-    @article = PandocRuby.convert(article, :from => :markdown, :to => :html)
+    article = PandocRuby.convert(article, :from => :markdown, :to => :html)
+
+    # met l'article dans le template et remplacer les "trous"
+    @article = template.gsub!("{content}", article)
+    @article.gsub!(/{[a-z]+}/) do |match|
+      name = /([a-z]+)/.match(match)
+      @config[name[1]]
+    end
+  end
+  def generate(input_path, output_path)
+    path = @path.sub(input_path, output_path)
+    Dir.mkdir(path)
+    File.open("#{path}/index.html", "w"){|file| file.write(@article)}
   end
 end
--- src/bloge.rb
+++ src/bloge.rb
@@ -18,25 +18,51 @@ class Bloge
     @articles = Dir["#{@input_path}/#{@config["articles"]}/*"]
       .map{|path| Article.new(path, "#{@input_path}/#{@config["article_template"]}")}
     # puts @articles.to_s
+
+    @content = ""
   end
 
   def generate
+    # detruit le repertoire de destination s'il existe
+    if File.directory?(@output_path)
+      FileUtils.rm_r(@output_path)
+    end
+
+    # cree le repertoire de destination
+    Dir.mkdir(@output_path)
+    Dir.mkdir("#{@output_path}/articles")
+    Dir.mkdir("#{@output_path}/ressources")
+    FileUtils.cp("#{@input_path}/ressources/style.css", "#{@output_path}/ressources/style.css")
+
+    # cree le fichier principal
     format_main
+    File.open("#{@output_path}/index.html", "w"){|file| file.write(@content)}
+
+    @articles.each{|article| article.generate(@input_path, @output_path)}
   end
 
   def format_main
     # charge le template et lance une exception s'il n'y a pas de nom
     template = @config["main_template"]
     raise "No template given" if template == ""
-    content = File.open("#{@input_path}/#{template}", "r"){|file| file.read}
+    @content = File.open("#{@input_path}/#{template}", "r"){|file| file.read}
+
+    # met les liens des articles
+    format_links
 
     # remplace les "trous" du template
-    content.gsub!("{title}",  @config["title"])
-    content.gsub!(/{[a-z]+}/) do |match|
+    @content.gsub!(/{[a-z]+}/) do |match|
       name = /([a-z]+)/.match(match)
       @config[name[1]]
     end
-    # File.open("Bidon.html", "w"){|file| file.write(content)}
+  end
+
+  def format_links
+    links = @articles.map do |i|
+      out = i.path.sub("#{@input_path}", ".")
+      "<a href='#{out}'>#{i.title}</a>"
+    end.join("\n")
+    @content.gsub!("{links}",  links)
   end
 end
 
